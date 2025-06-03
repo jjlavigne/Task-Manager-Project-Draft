ec22684d819f201245cc9cd54fc5ffa2ae77631e
jjlavigne <155919746+jjlavigne@users.noreply.github.com>
2025-06-02 22:30:21 -0700

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,100 +1,4 @@
-// Class that contains request operations for get, post, put, patch and delete
-class ApiClient {  
-    constructor(baseUrl) {       // Constructor gets base url for client
-        this.baseUrl = baseUrl;
-    }
-
-    queryResults = document.getElementById("response");  // Get text area to output response
-
-    // Delete request, asynchronous method
-    async  delete(id) {
-
-        let url = this.baseUrl + `/${id}`;  // Get record id
-
-        try {
-            await fetch(url, {         // Request delete
-                method: 'DELETE',
-            })
-            .then(response => {                                                                     // response returned from fetch
-                if (!response.ok) {                                                                 // HTTP request failed
-                    this.queryResults.value = 'HTTP DELETE request error: Status = ' + response.status.toString(); // log failure to Result output                                                  // Check for request error
-                    throw new Error('HTTP request failed: Status = ' + response.status.toString()); // Throw error POST,PUT or PATCH request failed
-                }})
-
-            this.queryResults.value = 'Deleted record number ' +  `${id}`;   // Send delete message to text area
-
-          // Catch error, send to console, and throw
-        } catch (error) {
-            console.error('DELETE', error);
-            throw error;
-        }
-    }
-
-    // Get request, asynchronous method
-    async get(id) {     // Pass id for specific record
-
-            let url = this.baseUrl;    // Start with base url
-            if (id) 
-                url += `/${id}`;       // Add id for specific record, if id was passed
-            
-            try {
-                await fetch(url)      // Request Get and return response
-                        .then(response => {                                                                     // response returned from fetch
-                            if (!response.ok) {                                                                 // HTTP request failed
-                                this.queryResults.value = 'HTTP GET request error: Status = ' + response.status.toString(); // log failure to Result output                                                  // Check for request error
-                                throw new Error('HTTP request failed: Status = ' + response.status.toString()); // Throw error stating that POST,PUT or PATCH request failed
-                            }
-                            return response;
-                            })
-                        .then((response) => response.json())   // Format response into json file and return file
-                        .then((json) => this.queryResults.value = JSON.stringify(json, null, 2))  // Stringify json file and return
-          
-            // Catch error, send to console, then throw error  
-            } catch (error) {
-                console.error('GET', error);
-                throw error;
-            }
-    }
-
-    // Post, Put and Patch requests, asynchronous method
-    // jsonFields: all fields with data for current method
-    // thisMethod: current method for this request (post, put or patch)
-    // ID: id of the record for put or patch request
-    async postPutPatch(jsonFields, thisMethod, id) {
-
-            try {
-                let url = this.baseUrl;     // Start with base url
-                if (id) 
-                    url += `/${id}`;      // Add id for specific record, if id was passed
-                await fetch(url, {
-                        method: thisMethod,    // Parameter: post, put or patch
-                        body: JSON.stringify(jsonFields),   // Parameter: all fields in json format for post, put or patch
-                        headers: {
-                            'Content-type': 'application/json; charset=UTF-8',   // Request header
-                        },
-                        })
-                        .then(response => {                                                                     // response returned from fetch
-                            if (!response.ok) {                                                                 // HTTP request failed
-                                this.queryResults.value = 'HTTP ' + thisMethod + ' request error: Status = ' + response.status.toString(); // log failure to Result output                                                   // Check for request error
-                                throw new Error('HTTP request failed: Status = ' + response.status.toString()); // Throw error stating that POST,PUT or PATCH request failed
-                            }
-                            return response;
-                            })
-                        .then((response) => response.json())    // Format response into json file and return file
-                        .then((json) => this.queryResults.value = JSON.stringify(json, null, 2));    // Stringify json file and return
-            
-            // Catch error, send to console, then throw error
-            } catch (error) {
-            console.error(thisMethod, error);
-            throw error;
-            }
-
-            const inputs = document.querySelectorAll('.fieldGroup input');
-    
-            inputs.forEach(input => {    // Clear all fields on client front end
-                input.value = '';});
-    }
-} 
+import { ApiClient } from './ApiClient.js';
 
 //-----------------------------------------------------------------------------------
 
@@ -106,7 +10,6 @@ document.getElementById("patch").addEventListener("change", radioPutPatch);
 document.getElementById("delete").addEventListener("change", radioGetDelete);
 // Event listener triggered when "Send Request" button is clicked
 document.getElementById("myButton").addEventListener("click", getRequestType);
-
 
 // Disables input for all input fields except for ID for Get and Delete requests
 function radioGetDelete() {
@@ -236,6 +139,7 @@ function postPutPatchRequest(method) {
     };
 
     const inputs = document.querySelectorAll('.fieldGroup input');  // Get all input fields from client front end
+    let currentJsonField = jsonFields;
 
     inputs.forEach(input => {                              // Check each input field for values
 
@@ -273,5 +177,11 @@ function postPutPatchRequest(method) {
         apiClient.postPutPatch(jsonFields, 'PUT', requestID.value);     // Send a PUT request to the apiClient
     else if (method === "Patch")
         apiClient.postPutPatch(jsonFields, 'PATCH', requestID.value);   // Send a PATCH request to the apiClient
+
+    
+    const clearInputs = document.querySelectorAll('.fieldGroup input');
+
+    clearInputs.forEach(input => {    // Clear all fields on client front end
+        input.value = '';});
 
 }
